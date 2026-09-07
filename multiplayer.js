e35dd8975a581589b4a3a30e27259dced65b5409
loured12 <loured12@protonmail.com>
2026-09-07 11:49:06 +0200

    Update multiplayer.js

--- multiplayer.js
+++ multiplayer.js
@@ -334,14 +334,6 @@
   };
   }
 
-  console.log(
-    "[DEBUG HOOK]",
-    "jouerCarte =",
-    typeof window.jouerCarte,
-    "started =",
-    started
-);
-
   for (const fn of ACTIONS)
     hook(fn);
 
