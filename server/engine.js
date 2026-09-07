92350f378f25d2e4115f0f7b45650dc1014d264c
loured12 <loured12@protonmail.com>
2026-09-07 11:20:32 +0200

    Remove debug console log from engine.js
    Removed debug logging for current player and hands.

--- server/engine.js
+++ server/engine.js
@@ -352,13 +352,6 @@ export class AtoumoulinEngine {
 
 const state = this.sandbox.__atoumoulinGetState();
 
-    console.log(
-    "[DEBUG TOUR]",
-    "fn =", fn,
-    "joueurActuel =", state.joueurActuel,
-    "mains =", state.joueurs.map(j => j.main.length)
-);
-
     if (
       fn === "jouerCarte" &&
       state &&
