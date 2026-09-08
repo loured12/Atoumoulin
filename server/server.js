985c7b59529e79deaa881c11e9662cffa8465fe7
loured12 <loured12@protonmail.com>
2026-09-08 13:48:58 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -402,11 +402,6 @@ if(m.type==="game:action"){
    [nouveauMode]
   );
 
-  console.log(
-   "DEBUG NOUVELLE PARTIE - VICTOIRES :",
-   room.engine.stateFor(player.index).victories
-);
-
   sendState(room);
 
   return;
