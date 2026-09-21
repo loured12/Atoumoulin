ee93370b537101c0ddc4af7789c55a0bd907e801
loured12 <loured12@protonmail.com>
2026-09-21 23:37:32 +0200

    Add event listener for choixBots to update bot count

--- script.js
+++ script.js
@@ -30,6 +30,11 @@ function mettreAJourNombreBots(){
 
 choixJoueurs.addEventListener("change", mettreAJourNombreBots);
 
+choixBots.addEventListener("change", () => {
+    globalThis.__atoumoulinNombreBots =
+        Number(choixBots.value) || 0;
+});
+
 mettreAJourNombreBots();
 
 const cartesBase = [
