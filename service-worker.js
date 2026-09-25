e7a7d5c72ca782cd210e8b85b64dda3e1e149f51
loured12 <loured12@protonmail.com>
2026-09-25 18:54:15 +0200

    Upgrade cache version and expand cached files
    Updated cache version and added more files to cache.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "atoumoulin-cache-v1";
+const CACHE_NAME = "atoumoulin-cache-v2";
 
 const FILES_TO_CACHE = [
   "./",
@@ -7,7 +7,31 @@ const FILES_TO_CACHE = [
   "./script.js",
   "./multiplayer.js",
   "./manifest.json",
-  "./1789034496969.png"
+  "./1789034496969.png",
+
+  "./cartes/01.png",
+  "./cartes/02.png",
+  "./cartes/03.png",
+  "./cartes/04.png",
+  "./cartes/05.png",
+  "./cartes/06.png",
+  "./cartes/07.png",
+  "./cartes/08.png",
+  "./cartes/09.png",
+  "./cartes/10.png",
+  "./cartes/11.png",
+  "./cartes/12.png",
+  "./cartes/13.png",
+  "./cartes/14.png",
+  "./cartes/15.png",
+  "./cartes/16.png",
+  "./cartes/17.png",
+  "./cartes/18.png",
+  "./cartes/19.png",
+  "./cartes/20.png",
+  "./cartes/21.png",
+  "./cartes/joker.png",
+  "./cartes/dos.png"
 ];
 
 self.addEventListener("install", event => {
