cdb27e197f8e9eec884c6750d0c9b5d3a679b72f
loured12 <loured12@protonmail.com>
2026-09-26 18:33:06 +0200

    Add appendChild method to document body

--- server/engine.js
+++ server/engine.js
@@ -56,6 +56,9 @@ function makeSandbox() {
   );
 
   const document = {
+    body: {
+      appendChild() {}
+    },
     getElementById(id) {
       if (!els.has(id)) els.set(id, element(id));
       return els.get(id);
